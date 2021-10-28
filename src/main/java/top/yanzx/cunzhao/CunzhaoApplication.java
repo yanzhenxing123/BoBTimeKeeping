f74daaae9b1e0968e1f62f2d0e212eb18008f820
yanzhenxing <zhenxing.yan@duobei.com>
2021-10-28 15:12:30 +0800

    project created

---
+++ src/main/java/top/yanzx/cunzhao/CunzhaoApplication.java
@@ -0,0 +1,13 @@
+package top.yanzx.cunzhao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CunzhaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CunzhaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/yanzx/cunzhao/CunzhaoApplicationTests.java
@@ -0,0 +1,13 @@
+package top.yanzx.cunzhao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CunzhaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
