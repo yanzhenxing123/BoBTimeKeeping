5ee1d4f4606c8b827885cb3c4c25a4766f3643af
yanzhenxing <zhenxing.yan@duobei.com>
2021-11-03 14:57:50 +0800

    feat: 项目初始化：配置文件

---
+++ src/main/java/top/yanzx/cunzhao/controller/UserController.java
@@ -0,0 +1,17 @@
+package top.yanzx.cunzhao.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/")
+    public String getUser(){
+        return "test";
+    }
+
+}
